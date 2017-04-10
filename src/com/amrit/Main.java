ff068ee5f347a506750d6bb2637abac73eba54c5
Amritpal <amritpals@users.noreply.github.com>
2017-04-10 15:26:31 +0530

    Create project java files

---
+++ src/com/amrit/Main.java
@@ -0,0 +1,128 @@
+package com.amrit;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.amrit.model.Employee;
+
+
+public class Main {
+
+	private static SessionFactory factory;
+
+	public static void setUp() {
+		try {
+			factory = new Configuration().configure().buildSessionFactory();
+		} catch (Throwable ex) {
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static void tearDown() {
+		factory.close();
+	}
+
+	/* Method to CREATE an employee in the database */
+	public Integer addEmployee(String fname, String lname, int salary) {
+		Session session = factory.openSession();
+		Transaction tx = null;
+		Integer employeeID = null;
+		try {
+			tx = session.beginTransaction();
+			Employee employee = new Employee(fname, lname, salary);
+			employeeID = (Integer) session.save(employee);
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return employeeID;
+	}
+
+	/* Method to READ all the employees */
+	public void listEmployees() {
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			List employees = session.createQuery("FROM Employee").list();
+			for (Iterator iterator = employees.iterator(); iterator.hasNext();) {
+				Employee employee = (Employee) iterator.next();
+				System.out.print("First Name: " + employee.getFirstName());
+				System.out.print("  Last Name: " + employee.getLastName());
+				System.out.println("  Salary: " + employee.getSalary());
+			}
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+	}
+
+	/* Method to UPDATE salary for an employee */
+	public void updateEmployee(Integer EmployeeID, int salary) {
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			Employee employee = (Employee) session.get(Employee.class, EmployeeID);
+			employee.setSalary(salary);
+			session.update(employee);
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+	}
+
+	/* Method to DELETE an employee from the records */
+	public void deleteEmployee(Integer EmployeeID) {
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			Employee employee = (Employee) session.get(Employee.class, EmployeeID);
+			session.delete(employee);
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+	}
+
+	public static void main(String[] args) {
+		Main m = new Main();
+		
+		Main.setUp();
+		
+		Integer emp1 = m.addEmployee("Bob", "Marley", 500);
+		Integer emp2 = m.addEmployee("Sushant", "Rajput", 1500);
+		
+		m.listEmployees();
+		
+		m.deleteEmployee(emp2);
+		
+		m.listEmployees();
+
+	}
+
+}
